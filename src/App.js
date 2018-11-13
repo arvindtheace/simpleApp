90b2f849f8f031fffa6b9bc8592c484a55469533
arvind <arvind@simility.com>
2018-11-13 17:40:09 +0530

    intial commit

--- src/App.js
+++ src/App.js
@@ -3,23 +3,41 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      input1: 0,
+      input2: 0,
+      result: 0
+    }
+  }
+  onAdd = () => {
+    this.setState({result: this.state.input1 + this.state.input2});
+  }
+  onMultiply = () => {
+    this.setState({result: this.state.input1 * this.state.input2});
+  }
+  onChangeInput1 = (e) => {
+    let input1 = parseInt(e.target.value);
+    input1 = isNaN(input1) ? 0 : input1; 
+    this.setState({input1});
+  }
+  onChangeInput2 = (e) => {
+    let input2 = parseInt(e.target.value);
+    input2 = isNaN(input2) ? 0 : input2; 
+    this.setState({input2});
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <label>Input 1</label>
+        <input type="text" value={this.state.input1} onChange={(e) => this.onChangeInput1(e, "input1")}/>
+        <label>Input 2</label>
+        <input type="text" value={this.state.input2} onChange={(e) => this.onChangeInput2(e, "input2")}/>
+        <label>Results</label>
+        <div>{this.state.result}</div>
+        <button onClick={this.onAdd}>Add</button>
+        <button onClick={this.onMultiply}>Multiply</button>
       </div>
     );
   }
